e9ab73ecaa916c9c0ad9ed21fae6aa4fadfedc39
Justyna Biel <justyna.biel@capgemini.com>
2022-09-19 22:42:48 +0200

    env

---
+++ src/environments/.environment.ts
@@ -0,0 +1,9 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiKey: '',
+  apiUrl: 'http://api.openweathermap.org/data/2.5',
+};
